1244313e7a0573a8f05485c6cdefac00603771ef
maltsevoff <alexmalczev@gmail.com>
2025-10-02 10:41:12 +0300

    init

---
+++ SpeechToTextDemo/SpeechToTextDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechToTextDemoApp.swift
+//  SpeechToTextDemo
+//
+//  Created by Aleksandr Maltsev on 02.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct SpeechToTextDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
